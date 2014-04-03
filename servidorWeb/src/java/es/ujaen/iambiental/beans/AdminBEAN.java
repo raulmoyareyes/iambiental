f9bcde570487b9d72494d151878651ce67d9ef8d
Raúl Moya Reyes <raulmoyareyes@hotmail.com>
2014-04-03 13:45:23 +0200

    SEW1 completado.

---
+++ servidorWeb/src/java/es/ujaen/iambiental/beans/AdminBEAN.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.ujaen.iambiental.beans;
+
+import javax.annotation.Resource;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author raulm
+ */
+@Component(value = "beanAdmin")
+public class AdminBEAN {
+
+}
---
+++ servidorWeb/src/java/es/ujaen/iambiental/recursos/SensorRECURSO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.ujaen.iambiental.recursos;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author raulm
+ */
+@Path("/sensor")
+public class SensorRECURSO {
+    
+}
