45060908aaba7c36c81b5d8aa0f332e4019ecfdb
Raúl Moya Reyes <raulmoyareyes@hotmail.com>
2014-04-14 18:16:51 +0200

    Avanzando en los modelos.

---
+++ servidorWeb/src/java/es/ujaen/iambiental/daos/ReglaProgramadaDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.ujaen.iambiental.daos;
+
+/**
+ *
+ * @author Raúl Moya Reyes <raulmoya.es>
+ */
+public class ReglaProgramadaDAO {
+    
+}
