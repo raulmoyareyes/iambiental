bdab35f25aeab58b7262ef6589cc908ebefc6eb4
AgustRuiz <agustinruizlinares@gmail.com>
2014-04-23 22:16:36 +0200

    SDS1 Diseño de la interfaz del programa simulador de sensores

---
+++ modeloSimulacion/src/es/ujaen/iambiental/modelosimulacion/MensajeUDP.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.ujaen.iambiental.modelosimulacion;
+
+/**
+ * @author Agustín Ruiz Linares <www.agustruiz.es>
+ */
+public class MensajeUDP {
+    
+    /**
+     * Constructor por defecto
+     */
+    public MensajeUDP(){
+        
+    }
+}
