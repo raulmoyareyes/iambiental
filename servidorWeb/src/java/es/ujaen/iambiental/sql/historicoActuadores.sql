f0d532b99fa42576ff217f6538d9a8cbfa207ce4
vicentepf <vicenteplazafranco@gmail.com>
2014-05-05 10:41:53 +0200

    Historico de actuadores - Tabla de historico - Disparador para insertar nuevas tuplas en historico antes de que se actualicen los datos en la tabla actuadores

---
+++ servidorWeb/src/java/es/ujaen/iambiental/sql/historicoActuadores.sql
@@ -0,0 +1,19 @@
+CREATE TABLE historicoactuadores (
+    id      INT,
+    dato    DOUBLE,
+    fecha   DATE
+);
+
+CREATE OR REPLACE TRIGGER historico_actuadores
+    BEFORE UPDATE ON sensores
+DECLARE
+    id_a    actuadores.id%TYPE;
+    dato_a  actuadores.dato%TYPE;
+    fecha   actuadores.fecha%TYPE;
+BEGIN
+    SELECT id, dato, fecha INTO id_a, dato_a, fecha_a
+    FROM    actuadores
+    INSERT INTO historicoactuadores (id, dato, fecha)
+        VALUES (id_a, dato_a, fecha_a);
+END;
+/
